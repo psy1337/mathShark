68cdd6ed39942dc451e3c644ac4edbdb764de544
Zaijan Gamulo <zaijan.gamulo@gmail.com>
2023-04-08 20:57:49 +0200

    make better wrong answers

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,44 @@ import { useState, useEffect } from "react"
 import styles from "./App.module.css"
 
 function App() {
+  const getRandomInt = (max: number) => {
+    return Math.floor(Math.random() * max)
+  }
+
+  const getWrongAnswer = (f1: number, f2: number): number => {
+    if (Math.random() < 0.5) {
+      f1++
+    } else {
+      f1--
+    }
+    let answer = f1 * f2
+    if (answer < 1) {
+      return f1
+    }
+    return f1 * f2
+  }
+
   const [playerAnswer, setPlayerAnswer] = useState("")
   const [factor1, setFactor1] = useState(getRandomInt(13))
   const [factor2, setFactor2] = useState(getRandomInt(13))
   const [correctAnswer, setCorrectAnswer] = useState(factor1 * factor2)
-  const [wrongAnswer1, setWrongAnswer1] = useState(getRandomInt(145))
-  const [wrongAnswer2, setWrongAnswer2] = useState(getRandomInt(145))
+  const [wrongAnswer1, setWrongAnswer1] = useState(
+    getWrongAnswer(factor1, factor2)
+  )
+  const [wrongAnswer2, setWrongAnswer2] = useState(
+    getWrongAnswer(factor2, factor1)
+  )
   const [randomAnswers, setRandomAnswers] = useState(
     [correctAnswer, wrongAnswer1, wrongAnswer2].sort(() => Math.random() - 0.5)
   )
-
-  function getRandomInt(max: number) {
-    return Math.floor(Math.random() * max)
-  }
 
   const loadAnswers = () => {
     setPlayerAnswer("")
     const f1 = getRandomInt(13)
     const f2 = getRandomInt(13)
     const cAnswer = f1 * f2
-    const wa1 = getRandomInt(145)
-    const wa2 = getRandomInt(145)
+    const wa1 = getWrongAnswer(f1, f2)
+    const wa2 = getWrongAnswer(f2, f1)
     setFactor1(f1)
     setFactor2(f2)
     setCorrectAnswer(cAnswer)
